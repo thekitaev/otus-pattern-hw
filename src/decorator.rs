1945f55ec5be937664a1683d43b10c1cd374d870
Andrei Kitaev <an.kitaev@vk.team>
2022-02-26 14:47:48 +0300

    fixed decorator

--- src/decorator.rs
+++ src/decorator.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)]
-
+use std::fmt::Debug;
 use std::time::Instant;
 
-pub trait Operation {
+trait Operation: Debug {
     fn perform(&self);
 }
 
-pub struct Notification {
+#[derive(Debug)]
+struct Notification {
     message: String,
 }
 
@@ -24,20 +25,24 @@ impl Operation for Notification {
     }
 }
 
-pub struct TimeIt<T: Operation> {
+#[derive(Debug)]
+struct TimeIt<T: Operation> {
     inner: T,
 }
 
-impl<T> TimeIt<T>
-where
-    T: Operation,
-{
-    pub fn new(inner: T) -> Self {
+impl<T: Operation> TimeIt<T> {
+    fn new(inner: T) -> Self {
         Self { inner }
     }
-    pub fn perform(&self) {
+}
+
+impl<T: Operation> Operation for TimeIt<T> {
+    fn perform(&self) {
         let start = Instant::now();
+        println!("starting {:?}", self.inner);
+
         self.inner.perform();
+
         let finish = Instant::now();
         let duration = finish - start;
         println!("operation took {}ns", duration.as_nanos())
@@ -46,11 +51,13 @@ where
 
 #[cfg(test)]
 mod test {
-    use crate::decorator::{Notification, TimeIt};
+    use crate::decorator::{Notification, Operation, TimeIt};
     #[test]
     fn test_decorator() {
         let notification = Notification::new("hello world");
+        notification.perform();
+
         let decorated = TimeIt::new(notification);
-        decorated.perform()
+        decorated.perform();
     }
 }
